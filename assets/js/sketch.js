4a927367df8e6510a00e58f8918cbb6b79013c0c
Kevin Malekera <r0900654@student.thomasmore.be>
2024-05-26 17:06:06 +0200

    Added camera control btn's and js functionalities

---
+++ assets/js/sketch.js
@@ -0,0 +1,35 @@
+const webcamElement = document.getElementById('webcam');
+const canvasElement = document.getElementById('canvas');
+const snapSoundElement = document.getElementById('snapSound');
+const webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement);
+
+const startBtn = document.querySelector('#start-button');
+const closeBtn = document.querySelector('#close-button');
+const flipBtn  = document.querySelector('#flip-button');
+const snapshotBtn = document.querySelector('#snapshot-button');
+
+startBtn.addEventListener('click', () => {
+
+  webcam.start()
+  .then(result =>{
+    console.log("webcam started");
+  })
+  .catch(err => {
+      console.log(err);
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  webcam.stop();
+});
+
+flipBtn.addEventListener('click', () => {
+  webcam.flip();
+  webcam.start();  
+});
+
+snapshotBtn.addEventListener('click', () => {
+  let picture = webcam.snap();
+  document.querySelector('#download-photo').href = picture;
+  document.querySelector('#download-photo').classList.remove('visually-hidden');
+});
