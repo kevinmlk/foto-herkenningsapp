f0d644cd5db61b6d60bd42e40058893b3163b176
Kevin Malekera <r0900654@student.thomasmore.be>
2024-05-27 15:43:45 +0200

    Added some styling and modified the display of the results

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -3,24 +3,22 @@ let modelHasLoaded = false;
 
 function modelLoaded() {
   console.log('Yay, the model has loaded!');
-  document.querySelector(".container h1").innerText = "click the image to classify";
+  document.querySelector("#title").innerText = "Model loaded!";
   modelHasLoaded = true;
 }
-
 
 document.querySelector('#photo').addEventListener('click', function (e) {
   console.log(e.target);
   if (modelHasLoaded && e.target instanceof HTMLImageElement) {
     classifier.classify(e.target, (err, results) => {
       console.log(results);
-      e.target.nextElementSibling.innerHTML = '';
+      // e.target.nextElementSibling.innerHTML = '';
       for (let result of results) {
         console.log(result);
-        if (result.confidence >= 0.1) {//0.8
-          e.target.nextElementSibling.innerHTML += `<h2>${result.label}</h2>`;
-        }
-        if (e.target.nextElementSibling.innerHTML === '') {
-          e.target.nextElementSibling.innerHTML = `<h2>no result found</h2>`
+        if (result.confidence >= 0.01) {//0.8
+          document.querySelector('#results').innerHTML += `<h2>${result.label}</h2>`;
+        } else {
+          document.querySelector('#results').innerHTML = `<h2>no result found</h2>`
         }
       }
 
